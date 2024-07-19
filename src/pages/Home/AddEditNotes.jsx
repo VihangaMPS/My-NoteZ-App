5f94df3789e597cdddccddd9717b8a1b8b0709c8
MPS Vihanga <>
2024-07-19 21:29:21 +0530

    Tags option added

--- src/pages/Home/AddEditNotes.jsx
+++ src/pages/Home/AddEditNotes.jsx
@@ -1,4 +1,11 @@
+import TagInput from "../../components/Input/TagInput.jsx";
+import {useState} from "react";
+
 const AddEditNotes = () => {
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [tags, setTags] = useState([]);
     return (
         <div>
             <div className="flex flex-col gap-2">
@@ -8,6 +15,8 @@ const AddEditNotes = () => {
                     type="text"
                     placeholder="Go to Gym"
                     className="text-2xl text-slate-950 outline-none"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
             </div>
 
@@ -17,11 +26,14 @@ const AddEditNotes = () => {
                     placeholder="Content"
                     className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
                     rows={10}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
                 />
             </div>
 
             <div className="mt-3">
                 <label className="input-label">TAGS</label>
+                <TagInput tags={tags} setTags={setTags}/>
             </div>
 
             <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>ADD</button>
