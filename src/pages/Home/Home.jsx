73d79e141386bf16516a2f17718972b58d3a8a80
MPS Vihanga <>
2024-07-20 00:15:12 +0530

    Modal window updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -51,7 +51,11 @@ const Home = () => {
                 contentLabel=""
                 className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 "
             >
-                <AddEditNotes/>
+                <AddEditNotes
+                    onClose={() => {setOpenAddEditModal({isShown: false, type: "add", data: null})}}
+                    type={openAddEditModal.type}
+                    noteData={openAddEditModal.data}
+                />
             </Modal>
         </>
     )
